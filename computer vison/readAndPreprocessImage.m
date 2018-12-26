a97907a5bab53ccd405d9a9e75a0e0426bdfc6e7
Howard Wang <3144017029@qq.com>
2018-12-26 14:38:06 +0000

    Delete readAndPreprocessImage.m

--- computer vison/readAndPreprocessImage.m
+++
@@ -1,9 +0,0 @@
-function Iout = readAndPreprocessImage(filename)
-    %Replicate the image 3 times to create an RGB image.
-   
-    I = imread(filename);
-    if ismatrix(I)
-        I = cat(3,I,I,I);
-    end
-    Iout = imresize(I, [224 224]);
-end
